3c12b9fd25deec9592760e84f528d2729877dc8f
CherryBlossomHope <1526566980@qq.com>
2023-03-14 23:49:24 +0800

    浅浅写个注释

--- src/hooks/keyboardEvents.ts
+++ src/hooks/keyboardEvents.ts
@@ -1,4 +1,9 @@
 import { onMounted, onUnmounted } from 'vue';
+/**
+ * 
+ * @param {Function} callBack 事件回调函数
+ * @param {string} key 键盘按键 &Enter
+ */
 export function useKeyboardEvents(callBack: Function, key: string) {
     onMounted(() => {
         window.addEventListener("keydown", keydown);
