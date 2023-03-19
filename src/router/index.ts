008795af02ea14ef0daec37307dd4a5d5fabddca
CherryBlossomHope <1526566980@qq.com>
2023-03-19 22:45:41 +0800

    对接接口

--- src/router/index.ts
+++ src/router/index.ts
@@ -53,24 +53,25 @@ const router = createRouter({
 })
 
 //判断登录状态 是否进入登录页面
-// router.beforeEach((to, from, next) => {
-//   console.log("😛 ~ file: index.ts:41 ~ router.beforeEach ~ to", to)
-//   console.log("🥰 ~ file: index.ts:41 ~ router.beforeEach ~ from", from)
-//   document.title = to.meta.title
-//   if (to.path == '/') {
-//     if (storageClass.get('USER_INFO').value) {
-//       getUserInfo({}).then(res => {
-//         // ElMessage({
-//         //   message: res.msg,
-//         //   type: 'success',
-//         // })
-//         next()
-//       })
-//     } else {
-//       next('/login')
-//     }
-//   } else {
-//     next()
-//   }
-// })
+router.beforeEach((to, from, next) => {
+  console.log("😛 ~ file: index.ts:41 ~ router.beforeEach ~ to", to)
+  console.log("🥰 ~ file: index.ts:41 ~ router.beforeEach ~ from", from)
+  document.title = to.meta.title
+  if (to.path == '/') {
+    if (storageClass.get('USER_INFO').value) {
+      // getUserInfo({}).then(res => {
+      //   // ElMessage({
+      //   //   message: res.msg,
+      //   //   type: 'success',
+      //   // })
+      //   // next()
+      // })
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
 export default router
